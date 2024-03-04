0e4fe3133aef383673dd0ceb498610b11b412d28
erenoske <erenisgoren@gmail.com>
2024-03-05 01:01:49 +0300

    Project kickoff: Personalized my working environment.

---
+++ DevProfile/Models/GithubManager.swift
@@ -0,0 +1,8 @@
+//
+//  GithubManager.swift
+//  DevProfile
+//
+//  Created by eren on 5.03.2024.
+//
+
+import Foundation
---
+++ DevProfile/Models/GithubUser.swift
@@ -0,0 +1,12 @@
+//
+//  Githup.swift
+//  DevProfile
+//
+//  Created by eren on 5.03.2024.
+//
+
+import Foundation
+
+struct Githup {
+    <#fields#>
+}
